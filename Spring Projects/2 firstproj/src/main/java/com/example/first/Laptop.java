c60b738d81ed5e97920a63bbb9101b45fafb7a6a
abhi5926 <abhisheksen5926@gmail.com>
2025-03-21 11:18:33 +0530

    [Abhishek] Add. SpringBoot Autowired Annotation is Used in project

---
+++ Spring Projects/2 firstproj/src/main/java/com/example/first/Laptop.java
@@ -0,0 +1,13 @@
+package com.example.first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void compile() {
+		System.out.println(" Compiling ");
+		
+	}
+
+}
